548cdb993653d7c4f3aa6bb3d7f98de8caedfec8
Azamkhan09 <165834289+Azamkhan09@users.noreply.github.com>
2025-06-22 16:00:25 +0300

    Update and rename JavaScript to main.js

--- main.js
+++ main.js
@@ -88,47 +88,14 @@ document.addEventListener('click', (event) => {
   }
 });
 
-function setCookie(name, value, days) {
-  let expires = "";
-  if (days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    expires = "; expires=" + date.toUTCString();
-  }
-  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-/**
- * Возвращает значение cookie с указанным именем.  Если cookie не существует, возвращает null.
- *
- * @param {string} name Имя cookie, которое нужно получить.
- * @returns {string | null} Значение cookie или null, если cookie не найден.
- */
-function getCookie(name) {
-  const nameEQ = name + "=";
-  const ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1, c.length);
-    }
-    if (c.indexOf(nameEQ) === 0) {
-      return c.substring(nameEQ.length, c.length);
-    }
-  }
-  return null;
-}
-
-/**
- * Удаляет cookie с указанным именем.
- *
- * @param {string} name Имя cookie, которое нужно удалить.
- */
-function eraseCookie(name) {
-  document.cookie = name+'=; Max-Age=-99999999;';
-}
-
-
-
 // Инициализация
 updateDisplay();
+
+// Установить cookie "token" с жизнью 30 днЁй
+setCookie('token', 'abc123', { 'max-age': 60 * 60 * 24 * 30 });
+
+// Получить cookie "token"
+console.log(getCookie('token')); // "abc123"
+
+// Удалить cookie "token"
+deleteCookie('token');
